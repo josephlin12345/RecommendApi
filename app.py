d94f6c31570cfe06913a37a44c75fa6d45c05cee
LinJoseph <josephlin12345@gmail.com>
2021-07-17 12:05:35 +0800

    update event & add tag

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from datetime import datetime
 
-# import tensorflow_hub as hub
-# import tensorflow_text
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_restful import Api, Resource, reqparse
-from pymongo import MongoClient, UpdateOne
-
-# load sentence encoder
-# url = 'universal-sentence-encoder-multilingual-large_3'
-# embed = hub.load(url)
+from pymongo import DESCENDING, MongoClient, UpdateOne
 
 # init app
 app = Flask(__name__)
@@ -19,117 +13,119 @@ api = Api(app)
 # init db
 client = MongoClient('mongodb+srv://api:gPdCEpVRVWuOnGqp@cluster0.xgkp9.mongodb.net/?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority')
 db = client['recommended_system']
-user_db = db['user']
-tag_db = db['tag']
-event_db = db['event']
-establisher_db = db['establisher']
+user_collection = db['user']
+event_collection = db['event']
+tag_collection = db['tag']
 
 def toDate(dateString):
   return datetime.strptime(dateString, '%Y-%m-%dT%H:%M:00.000Z')
 
 class User(Resource):
-  def get(self):
-    parser = reqparse.RequestParser()
-    parser.add_argument('id', required=True, help='id is required', type=str)
-    args = parser.parse_args()
+  def __init__(self):
+    self.parser = reqparse.RequestParser()
+    self.parser.add_argument('_id', required=True, help='_id(str) is required', type=str)
 
-    data = user_db.find_one(args['id'])
+  # temp
+  def get(self):
+    args = self.parser.parse_args()
+
+    data = user_collection.find_one(args['_id'])
     if data:
-      return jsonify(data)
+      return jsonify({ 'result': data })
     else:
-      return jsonify({})
+      return jsonify({ 'error': f'_id {args["_id"]} not exist' })
 
   def post(self):
-    parser = reqparse.RequestParser()
-    parser.add_argument('id', required=True, help='id is required', type=str, location='json')
-    parser.add_argument('pageTitle', required=True, help='pageTitle is required', type=str, location='json')
-    args = parser.parse_args()
+    self.parser.add_argument('pageTitle', required=True, help='pageTitle(str) is required', type=str)
+    args = self.parser.parse_args()
 
     now = datetime.now()
-    user_db.update_one(
-      { '_id': args['id'] },
+    result = user_collection.update_one(
+      { '_id': args['_id'] },
       {
         '$push': {
           f'pageTitles.{now.strftime("%Y-%m-%d")}': {
             'pageTitle': args['pageTitle'],
-            # 'encodedPageTitle': embed([args['pageTitle']]).numpy().tolist()[0],
             'timestamp': now
           }
         }
       },
       upsert=True
     )
-    return args
+    if result.modified_count or result.upserted_id:
+      return jsonify({ 'result': f'_id {args["_id"]} updated' })
+    else:
+      return jsonify({ 'error': 'something went wrong' })
 
 class Event(Resource):
-  def post(self):
-    parser = reqparse.RequestParser()
-    parser.add_argument('establisher', required=True, help='establisher is required', type=str, location='json')
-    parser.add_argument('title', required=True, help='title is required', type=str, location='json')
-    parser.add_argument('url', default=None, type=str, location='json')
-    parser.add_argument('description', default=None, type=str, location='json')
-    parser.add_argument('startDate', default=None, type=toDate, location='json')
-    parser.add_argument('endDate', default=None, type=toDate, location='json')
-    parser.add_argument('image', default=None, type=str, location='json')
-    parser.add_argument('tags', default=None, action='append', type=str, location='json')
-    args = parser.parse_args()
+  def __init__(self):
+    self.parser = reqparse.RequestParser()
+    self.parser.add_argument('establisher', required=True, help='establisher(str) is required', type=str)
+    self.parser.add_argument('title', required=True, help='title(str) is required', type=str)
+    self.parser.add_argument('url', help='url(str)', type=str)
+    self.parser.add_argument('description', help='description(str)', type=str)
+    self.parser.add_argument('startDate', help='%Y-%m-%dT%H:%M:00.000Z', type=toDate)
+    self.parser.add_argument('endDate', help='%Y-%m-%dT%H:%M:00.000Z', type=toDate)
+    self.parser.add_argument('image', help='image(str)', type=str)
+    self.parser.add_argument('tags', help='tags(list of str)', action='append', type=str)
 
-    # find establisher lastEventId
-    establisher = establisher_db.find_one({ 'name': args['establisher'] })
-    if establisher == None:
-      establisher_db.insert_one({
-        'name': args['establisher'],
-        'lastEventId': 0
-      })
-      establisher = establisher_db.find_one({ 'name': args['establisher'] })
-    lastEventId = establisher['lastEventId'] + 1
+  def post(self):
+    args = self.parser.parse_args()
+
+    # find lastEventId
+    try:
+      lastEvent = list(event_collection.find().sort('_id', DESCENDING).limit(1))[0]
+      lastEventId = lastEvent['_id']
+    except:
+      lastEventId = 0
 
     now = datetime.now()
-    event_db.update_one(
-      { '_id': f'{args["establisher"]}-{lastEventId}' },
+    doc = args
+    doc['_id'] = lastEventId + 1
+    doc['modifyDate'] = now
+    doc['createDate'] = now
+    result = event_collection.insert_one(doc)
+    if result.inserted_id:
+      return jsonify({ 'result': f'_id {result.inserted_id} inserted' })
+    else:
+      return jsonify({ 'error': 'something went wrong' })
+
+  def patch(self):
+    self.parser.add_argument('_id', required=True, help='_id(int) is required', type=int)
+    args = self.parser.parse_args()
+
+    now = datetime.now()
+    doc = { arg: value for arg, value in args.items() if value != None}
+    doc['modifyDate'] = now
+    result = event_collection.find_one_and_update({ '_id': args['_id'] }, { '$set': doc })
+    if result:
+      return jsonify({ 'result': f'_id {args["_id"]} updated' })
+    else:
+      return jsonify({ 'error': f'_id {args["_id"]} not exist' })
+
+class Tag(Resource):
+  def post(self):
+    parser = reqparse.RequestParser()
+    parser.add_argument('tags', required=True, help='tags(list of str) is required', action='append', type=str)
+    args = parser.parse_args()
+
+    now = datetime.now()
+    result = tag_collection.bulk_write([UpdateOne(
+      { 'name': tag },
       {
-        '$set': {
-          'establisher': args['establisher'],
-          'title': args['title'],
-          # 'encodedTitle': embed([args['title']]).numpy().tolist()[0],
-          'url': args['url'],
-          'description': args['description'],
-          'startDate': args['startDate'],
-          'endDate': args['endDate'],
-          'image': args['image'],
-          'tags': args['tags'],
-          'modifyDate': now
-        },
-        '$setOnInsert': {
-          'createDate': now
-        }
+        '$set': { 'lastUsedDate': now },
+        '$setOnInsert': { 'createDate': now }
       },
       upsert=True
-    )
-    establisher_db.update_one({ 'name': args['establisher'] }, { '$set': { 'lastEventId': lastEventId } })
-    if args['tags']:
-      bulk_requests = []
-      # embeded = embed(args['tags']).numpy().tolist().reverse()
-      for tag in args['tags']:
-        bulk_requests.append(
-          UpdateOne(
-            { 'name': tag },
-            {
-              '$set': {
-                'lastUsedDate': now
-              },
-              '$setOnInsert': {
-                # 'encodedName': embeded.pop(),
-                'createDate': now
-              }
-            },
-            upsert=True
-          )
-        )
-      tag_db.bulk_write(bulk_requests)
+    ) for tag in args['tags']])
+    if result.modified_count or result.upserted_count:
+      return jsonify({ 'result': f'{result.modified_count} tags updated & {result.upserted_count} tags inserted' })
+    else:
+      return jsonify({ 'error': 'something went wrong' })
 
 api.add_resource(User, '/api/user')
 api.add_resource(Event, '/api/event')
+api.add_resource(Tag, '/api/tag')
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
