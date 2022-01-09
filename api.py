9261e09563e31817e0012ee94e73109d6d05c923
LinJoseph <josephlin12345@gmail.com>
2022-01-09 22:46:01 +0800

    change language to none

--- api.py
+++ api.py
@@ -281,7 +281,7 @@ class Event(Resource):
 
       query = {}
       if args.get('q', None):
-        query['$text'] = { '$search': args['q'] }
+        query['$text'] = { '$search': args['q'], '$language': 'none' }
       if args['type'] == 'user':
         query['establisher'] = args['email']
 
